0382a4ce6c4fa43b98735cc1721cb315d656cae5
Nabiel Alif <nabiel.alif01@gmail.com>
2022-12-03 14:19:03 +0700

    style: button component

---
+++ client/src/components/Button.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  props: {
+    cancel: Boolean,
+    draft: Boolean,
+    submit: Boolean,
+  },
+};
+</script>
+<template>
+  <button v-if="cancel">Cancel</button>
+  <button v-else-if="draft" class="border border-grayish px-5 py-2.5 rounded-md bg-white">Save As Draft</button>
+
+  <button v-else-if="submit" class="bg-primary rounded-md px-20 py-2.5 text-white ">Submit</button>
+</template>
